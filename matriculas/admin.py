aaa073c8909934d1e2a90763ab5a4005240af1a0
Kauan Santana Barros <kauansbflu@gmail.com>
2024-07-12 15:06:25 -0300

    Update admin.py

--- matriculas/admin.py
+++ matriculas/admin.py
@@ -3,12 +3,12 @@ from matriculas.models import Matricula, User, Curso
 
 
 class MatriculaAdmin(admin.ModelAdmin):
-    list_display = ('aluno','curso','data_matricula')
-    search_fields = ('aluno', 'curso')
+    list_display = ('aluno','curso','data_matricula',)
+    search_fields = ('aluno__nome', 'curso__nome', 'data_matricula',)
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('nome','email')
+    list_display = ('nome','email',)
     search_fields = ('nome',)
 
 
